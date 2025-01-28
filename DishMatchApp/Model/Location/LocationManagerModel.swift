0e182912502ab90e9deb90b230bd3d7a2f5f59b9
H4KuuuA <kd1419898@st.kobedenshi.ac.jp>
2025-01-28 14:12:42 +0900

    LocationManager classをfinal classに変更

--- DishMatchApp/Model/Location/LocationManagerModel.swift
+++ DishMatchApp/Model/Location/LocationManagerModel.swift
@@ -8,7 +8,7 @@
 import Foundation
 import CoreLocation
 
-class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+final class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
     static let shared = LocationManager() // シングルトンインスタンス
 
     private var locationManager = CLLocationManager()
